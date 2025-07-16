623ce6053c53c5367dd6357efa21b3a5486a5a0c
prxblms <malcondev.lds@hotmail.com>
2025-07-16 16:39:40 -0300

    login_ok

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='login'),
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='loging')
 ]
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 
 def index(request):
@@ -7,5 +8,24 @@ def index(request):
 
 def register(request):
     return render(request, 'accounts/register.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return render(request, 'index.html', {'error': 'Usuário ou senha inválidos'})
+
+    return render(request, 'index.html')
+
+
+
 
 
