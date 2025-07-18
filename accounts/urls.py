6e0f51be0ec00d930a35912db0002843e816c9cf
prxblms <malcondev.lds@hotmail.com>
2025-07-18 10:33:19 -0300

    'restart_login'

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='login'),
-    path('register/', views.register, name='register'),
-    path('login/', views.access_login, name='access_login'),
-    path('register/user', views.register_user, name='register_user'),
-    path('logout/', views.logout, name='logout'),
+    path('', views.login_user, name='login'),
+    path('register/', views.register_user, name='register'),
+    path('logout/', views.logout_user, name='logout'),
 ]
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,51 +1,14 @@
-from django.contrib.auth import authenticate, login, logout as site_logout
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .forms import RegistroForm
 
 
-def index(request):
-    return render(request, 'accounts/index.html')
-
-@login_required
-def register(request):
-    return render(request, 'accounts/register.html')
-
-
-def access_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('list')
-        else:
-            return render(request, 'index.html', {'error': 'Usuário ou senha inválidos'})
-
-    return render(request, 'index.html')
+def login_user(request):
+    return render(request, 'accounts/login.html')
 
 
 def register_user(request):
-    if request.method == 'POST':
-        form = RegistroForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = RegistroForm()
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html')
 
 
-def logout(request):
-    site_logout(request)
-    return redirect('login')
-
-
-
-
-
-
+def logout_user(request):
+    return render(request, 'accounts/logout.html')
 
